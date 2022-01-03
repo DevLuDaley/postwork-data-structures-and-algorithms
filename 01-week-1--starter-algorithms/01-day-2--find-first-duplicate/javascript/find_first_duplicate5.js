6d55279dcd3b97a6a1173df17ab2c59b8478c7ba
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2022-01-03 17:06:19 -0500

    solve algo again

---
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate5.js
@@ -0,0 +1,69 @@
+function findFirstDuplicate(arr) {
+// Javascript program to find first
+// repeating element in arr[]
+//! what info do I have?
+// arr =[2,1,3,3,2]
+//
+//! what do I need to define?
+// define duplicate as a number that has more than one instance.
+// . meaning it appears twice
+//
+//
+
+  // !steps
+  // push each element into a newArray=[]. If the currentElement already exists in newArray then return currentElement
+
+  //
+  const box = [];
+  let currentElement;
+  for (let i = 0; i < arr.length; i++) {
+    currentElement = arr[i];
+    if (box.includes(currentElement)) {
+      return currentElement;
+    }
+    box.push(currentElement);
+    // if (!box.includes(currentElement)) {
+    //   return -1;
+    // }
+  }
+  return -1;
+  // return "No duplicates here!";
+}
+findFirstDuplicate([2, 1, 3, 3, 2]); // 3
+// findFirstDuplicate([2, 1, 3, 7, 5]); // 3
+// findFirstDuplicate([2, 1, 5, 2, 3, 3, 2]); // 2
+// findFirstDuplicate([2, 1, 5, 3, 2, 3, 3, 2]); // 2
+// findFirstDuplicate([10, 5, 3, 4, 3, 5, 6]); // 3
+findFirstDuplicate([10, 5, 3, 4, 5, 3, 6]); // 5
+// findFirstDuplicate([2, 1, 55, 4, 9, 55, 2, 3, 3, 2]); // 55
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log('Expecting: 3');
+  console.log('Returning =>', findFirstDuplicate([2, 1, 3, 3, 2]));
+
+  console.log('');
+
+  console.log('Expecting: -1');
+  console.log('Returning =>', findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log('Expecting: 2');
+  console.log('Returning =>', findFirstDuplicate([1, 3, 4, 2, 2]));
+
+  console.log('Expecting: 3');
+  console.log('Returning =>', findFirstDuplicate([3, 1, 3, 4, 2]));
+}
+
+module.exports = findFirstDuplicate;
+
+/*
+1. Rewrite the problem in your own words
+2. Validate that you understand the problem
+3. Write your own test cases
+4. Pseudocode
+5. Code!
+*/
+
+// Please add your pseudocode to this file
+
+// And a written explanation of your solution
