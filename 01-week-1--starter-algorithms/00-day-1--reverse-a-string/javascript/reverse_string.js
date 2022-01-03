1043499d9a60df113008f1993e8b4288923f43b0
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2022-01-03 17:07:48 -0500

    add to console message

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -2,18 +2,18 @@ function reverseString(str) {
   // type your code here
   // console.log(str.split("").reverse().join())
   // console.log(str.split("").reverse().join(""))
-  return str.split("").reverse().join("")
+  return str.split('').reverse().join('');
 }
-reverseString("ybabtac")
+reverseString('ybabtac');
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log('Returning =>', reverseString('hi'));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log('Returning =>', reverseString('catbaby'));
 }
 
 module.exports = reverseString;
@@ -22,17 +22,16 @@ module.exports = reverseString;
 /*
 ! brainstorming
 [] what is the input? // a string
-      [] does it need to be cleaned? 
+      [] does it need to be cleaned?
         [] does case matter?
       [] if case matters then I can use "toLower" method to make all letters lowercase
           [] - no mention of case in directions - converting case might cause the algorithm to fail tests.
 [] what is the output? // a reversed version of the string
 
-
 can I call reverse on it? // no
 reverse is an array function
 can I convert the string into an array, then reverse the array, then convert(join) back to a string? // yes
-which functions/methods whould I need to convert/reverse/convert back to an array? // split/reverse/join 
+which functions/methods whould I need to convert/reverse/convert back to an array? // split/reverse/join
 
 */
 // And a written explanation of your solution
