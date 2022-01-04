c93b2e46f4b64531d71e5b03a3503e4cc87725f1
devludaley <daleyluis@gmail.com>
2022-01-04 15:54:43 -0500

    Update find_shortest_string_recursive.js

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,21 +1,58 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  arr;
+  // arr.length === 1 ? arr[0] : null;
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  const result = findShortestStringRecursive(arr.slice(1));
+
+  return arr[0].length <= result.length ? arr[0] : result;
 }
+
+// ! iterative answer below
+// function findShortestString(arr) {
+//   // type your code here
+//   arr;
+//   const sorted = arr.sort((a, b) => a.length - b.length);
+//   return sorted[0];
+// }
+
+/*
+iterative solution would be to use a for loop to iterate over arr comparing the .length of each element in the array and returning the smallest one. there must be a go to for comparing elements. reduce maybe? maybe recursion is best for comparison?
+
+let lowest = 0
+arr.forEach((str)=>
+str.length === 1
+return str
+
+// else if str.length !== 1 && str.length < lowest
+else if (str.length > lowest){
+  lowest = str.length
+}
+{
+
+}
+)
+*/
+// }
+// findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']);
+// findShortestStringRecursive(['cat', 'hi', 'dog', 'an']);
+findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']);
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
+  console.log('=>', findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
+  console.log('=>', findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+  console.log('=>', findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
 }
 
 module.exports = findShortestStringRecursive;
